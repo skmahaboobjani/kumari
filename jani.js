86444194919a1e1ed1f82207ebab95c55f6f247f
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2023-03-20 09:48:04 +0530

    Add files via upload

---
+++ jani.js
@@ -0,0 +1,103 @@
+// global scope-->let const var work
+
+//block scope --->let and const work
+
+//function scope---> var work
+
+//globalscope
+
+//let and const var
+
+// let a=10
+// var b=20
+// const c=30
+// function name()
+// {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+//     let d=20
+//     let x=10
+//     if(x>3)
+//     {
+//         let m=20
+//         // console.log(m);
+//         console.log("true");
+//     }
+//     else{
+//         console.log("false");
+//     }
+//     console.log(d);
+// }
+// name()
+
+//differnce b/w let and const var
+
+// var and let
+
+// var a=20
+//  var a=10
+// console.log(a);
+// console.log(a);
+
+// var before es5 var
+
+// let and const es6 version
+
+// let b=10 
+
+// b=20
+// console.log(b)
+
+
+//const cannot assign value
+
+// const a=10
+//   a=20
+// console.log(a)
+
+//java datatypes string 
+
+//javascipt
+
+// let mabu="jani"
+// console.log(mabu);
+
+
+// data types
+//null -->absence of no value considered  as null
+//string --> "",''
+//undefined --> variables let const var decalre  u did not assign the value is called undefined
+//big int--->largr number of numbers
+//boolean
+
+//it is not a data types
+//not defined--> let const var here they didnot declare and trying to acces the vlaue considered as not definded
+
+
+
+//  a
+// console.log(a);
+
+let a=null
+console.log(a);
+
+//  resource for javacsript https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+
+let m=true
+
+console.log(typeof(m));
+
+
+
+///hoisting
+//clousure
+//promises
+//callbacks
+//high order functions
+//iffe
+// arrow
+//anonymous functions
+//
